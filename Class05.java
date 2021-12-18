cc55bf79c9fc1ee4fe989d2005962b7d75574bdf
109b29769 <91443156+109b29769@users.noreply.github.com>
2021-12-18 14:05:52 +0800

    Add files via upload

---
+++ Class05.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Class05 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO �۰ʲ��ͪ���k Stub
+		char data[]=new char[128];
+		FileReader fr=new FileReader("c:\\Java\\donkey.txt");
+		fr.skip(9);
+		int num=fr.read(data);
+		String str=new String(data,0,num);
+		System.out.println("Characters read= "+num);
+		System.out.println(str);
+		fr.close();
+	}
+
+}
